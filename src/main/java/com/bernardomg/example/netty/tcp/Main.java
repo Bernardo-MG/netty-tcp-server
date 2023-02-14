1d3a4ba058ebaa030c3ef7eaa3ac83b064b3846f
Bernardo-MG <programming@bernardo-mg.com>
2023-02-14 23:13:33 +0100

    Cleaned up code

--- src/main/java/com/bernardomg/example/netty/tcp/Main.java
+++ src/main/java/com/bernardomg/example/netty/tcp/Main.java
@@ -3,20 +3,12 @@ package com.bernardomg.example.netty.tcp;
 
 import com.bernardomg.example.netty.tcp.cli.TcpServerMenu;
 
-import lombok.extern.slf4j.Slf4j;
 import picocli.CommandLine;
 
-@Slf4j
 public class Main {
 
     public static void main(final String[] args) {
-        final Integer exitCode;
-
-        exitCode = new CommandLine(new TcpServerMenu()).execute(args);
-
-        // log.debug("Exited with code {}", exitCode);
-        //
-        // System.exit(exitCode);
+        new CommandLine(new TcpServerMenu()).execute(args);
     }
 
     public Main() {
